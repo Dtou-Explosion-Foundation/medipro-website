a9ed7d19f33dbebd02bd20a099dd4425d9922194
hel-kun <farflylonglight@gmail.com>
2024-02-20 22:06:46 +0900

    websiteの移行

---
+++ tailwind.config.mjs
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				footergray: {
+				  light: '#60739f',
+				  DEFAULT: '#60739f',
+				  dark: '#c5c9d4',
+				},
+				footerhovergray: {
+				  light: '#222939',
+				  DEFAULT: '#222939',
+				  dark: '#f5f7fc',
+				},
+			},
+			listStyleType: {
+				circle: 'circle',
+				square: 'square',
+				roman: 'upper-roman',
+			},
+			screens: {
+				x3lmax: { max: "2550px" },
+				// => @media (max-width: 1535px) { ... }
+				
+				x2lmax: { max: "1650px" },
+				// => @media (max-width: 1279px) { ... }
+				
+				xlmax: { max: "1279px" },
+				// => @media (max-width: 1279px) { ... }
+				
+				lgmax: { max: "1023px" },
+				// => @media (max-width: 1023px) { ... }
+				
+				mdmax: { max: "767px" },
+				// => @media (max-width: 767px) { ... }
+				
+				smax: { max: "639px" },
+				// => @media (max-width: 639px) { ... }
+			},
+		},
+	},
+	plugins: [],
+}
